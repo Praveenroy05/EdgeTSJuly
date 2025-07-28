9cd769f8a509b2eaeaf89e1ceb518b03984d82f2
Praveen <praveen.k@xnara.com>
2025-07-28 20:04:10 +0530

    added browser and page fixture

---
+++ tests/browserAndPageFixture.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test'
+
+test("Understanding browser fixture", async function({browser}){
+
+    const context = await browser.newContext() // Created a browser context
+    const page = await context.newPage()
+})
+
+test("Understanding page fixture", async function({page}){
+
+})
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,44 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test'
+
+//const {test, expect} = require('@playwright/test')
+
+// test() - It helps us in declaration of a testcase
+// expect() - It helps us in assertion of a testcase/test step
+
+// Test File - That will always go inside the tests folder
+// fileName.spec.ts OR fileName.test.ts
+
+
+test("Validate login with valid credentials", async ({page})=>{
+
+  // page , browser - Fixture - Which helps us in setting up and tear down automatically
+
+
+  // Launch the browser
+  // launch the url 
+  // fill the username  - Locators
+  // fill the password
+  // click the login button
+  // Validate if the user is on the home page
+})
+
+test("Invalid Login Test", ()=>{})
+
+
+
+  // Title - Validate login with valid credentials
+  // Launch the browser
+  // launch the url 
+  // fill the username 
+  // fill the password
+  // click the login button
+  // Validate if the user is on the home page
+
+
+
+
+
+
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -7,13 +47,27 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link',   async function({ page }) {
   await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
+  await console.log("Launch the url");
   await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
+  await console.log("Click on the get started");
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await console.log("validated");
 });
 
+
+test("Login into the application", async ()=>{
+  // launch the url - Terminated over here in selenium
+  // fill the username 
+  // fill the password
+  // click the login button
+})
+
+// Websocket(Playwright) and http (selenium)
+
+
+
+
+
+// export - import
